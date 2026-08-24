ee35f00b496e9be5090d2e531f7d078595b373e0
xayd001 <zaydibnmukhtar01@gmail.com>
2026-08-24 14:59:44 +0100

    Add assetsInclude pattern for uppercase image extensions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.RENDER ? '/' : '/yaysib-exam-portal/'
+  base: process.env.RENDER ? '/' : '/yaysib-exam-portal/',
+  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.PNG', '**/*.png']
 });
- 
