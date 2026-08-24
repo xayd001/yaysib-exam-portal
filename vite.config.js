85ab8300cf6caf34153a892efe461bd1a4b19f44
xayd001 <zaydibnmukhtar01@gmail.com>
2026-08-24 14:35:18 +0100

    Simplify vite config and ensure devDependencies for Render

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.RENDER ? '/' : '/yaysib-exam-portal/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
+  base: process.env.RENDER ? '/' : '/yaysib-exam-portal/'
 });
+ 
