faa9b150960007207a8e0e19691d10db2416b7bd
xayd001 <zaydibnmukhtar01@gmail.com>
2026-08-25 20:31:04 +0100

    Fix logoImg variable naming in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import questionsData from './data/questions.json';
-import LogoImg from './assets/YCB.JPG'; 
+import logoImg from './assets/YCB.JPG'; 
 
 const adminUsername = import.meta.env.VITE_ADMIN_USER;
 const adminPassword = import.meta.env.VITE_ADMIN_PASS;
